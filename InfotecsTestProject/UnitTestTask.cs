c7c5f97b9a774a2bcbd53866959296d9f333a638
Mikhail Zhukovich <zhukovich.m.572-2@e.tusur.ru>
2024-01-24 18:50:18 +0700

    Test is finished

--- InfotecsTestProject/UnitTestTask.cs
+++ InfotecsTestProject/UnitTestTask.cs
@@ -1,5 +1,6 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 
@@ -19,8 +20,23 @@ namespace InfotecsTestProject
         [Test]
         public void Test1()
         {
-            driver.Navigate().GoToUrl("https://www.google.com/");
-            Assert.Pass();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl("https://www.gosuslugi.ru/");
+            wait.Until(driver => driver.FindElement(By.XPath(".//button[contains(text(),'Войти')]")))
+                .Click();
+            wait.Until(driver => driver.FindElement(By.XPath(".//button[contains(text(),'Не удаётся войти?')]")))
+                .Click();
+            wait.Until(driver => driver.FindElement(By.XPath(".//button[contains(text(),'восстановления пароля')]")))
+                .Click();
+            if (wait.Until(driver => driver.FindElement(By.XPath(".//*[contains(text(),'Восстановление пароля')]")))
+                               .Displayed)
+            {
+                Assert.Pass("Открылась страница восстановления пароля");
+            }
+            else
+            {
+                Assert.Fail("Не открылась страница восстановления пароля");
+            }
         }
     }
 }
